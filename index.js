7c428da74820eecbdb34daa92ba94bb3f23fdb95
Omar <otantawy53@gmail.com>
2022-04-28 10:08:33 -0400

    commit tree a5a2dabdd5b058dc3449cd73d30eb0187215c72d

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// code your solution here
+
+function winFinder(element){
+    return element.result === 'W'
+}
+
+// function superbowlWin(array){
+//     let result = array.find(winFinder);
+//     if (result){
+//         //console.log(result);
+//         return result.year;
+//     }
+// }
+
+// const superbowlWin = (array) => {
+//     const win = array.find( ({result}) => result === 'W');
+//     return !!win ? win.year : undefined;
+// }
+
+const superbowlWin = (array) => {
+    const win = array.find(() => array.result === 'W');
+    return !!win ? win.year : undefined;
+}
+
+
+// const superbowlWin = array => {
+//     let result = array.find(winFinder);
+//     if (result){
+//         //console.log(result);
+//         return result.year;
+//     }
+// }
+
+//what is being called into the function? what portion
+//of the callback function is being pulled in?
+//if i wanted to write the function within the superbowlwin function
+//what would that look like?
+
+//if i wanted to console.log the result values from the array, how would i do that
+
+// function superbowlWin(array){
+//     let result = function(element){
+//         return element.result === 'W'
+//     }
+//     if (result){
+//         // console.log(result);
+//         return result.year;
+//     }
+// }
+
+//******************************************************************//
+
+// function superbowlWin(element){
+//     return (element === 'W');
+// }
+
+// console.log(record.find(superbowlWin));
